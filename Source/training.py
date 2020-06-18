7cce8b8983095d5615ff4db0fc7943f1d9006957
Philipp Hellmich <runningman84@users.noreply.github.com>
2020-06-18 15:55:45 +0200

    Update training.py

--- Source/training.py
+++ Source/training.py
@@ -74,7 +74,7 @@ training_params = \
     },
     "ResourceConfig": {
         "InstanceCount": 1,
-        "InstanceType": "ml.p3.2xlarge",
+        "InstanceType": "ml.p3.8xlarge",
         "VolumeSizeInGB": 50
     },
     "TrainingJobName": job_name,
